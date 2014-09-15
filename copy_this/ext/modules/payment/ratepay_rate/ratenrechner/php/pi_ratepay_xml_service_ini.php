d762baafe625ddd21430a990005510f3d2b790ab
caramba1337 <soeren.martius@googlemail.com>
2014-09-15 14:57:02 +0200

    PRINT-73 Gateway URLs bei allen Anbindungsarten anpassen

--- copy_this/ext/modules/payment/ratepay_rate/ratenrechner/php/pi_ratepay_xml_service_ini.php
+++ copy_this/ext/modules/payment/ratepay_rate/ratenrechner/php/pi_ratepay_xml_service_ini.php
@@ -8,6 +8,6 @@
  * @package pi_ratepay_rate_calculator
  * Code by PayIntelligent GmbH  <http://www.payintelligent.de/>
  */
-$pi_ratepay_live_url = 'https://webservices.eos-payment.com/custom/ratepay/xml/1_0';
-$pi_ratepay_sandbox_url = 'https://webservices-int.eos-payment.com/custom/ratepay/xml/1_0';
+$pi_ratepay_live_url = 'https://gateway.ratepay.com/api/xml/1_0';
+$pi_ratepay_sandbox_url = 'https://gateway-int.ratepay.com/api/xml/1_0';
 ?>
